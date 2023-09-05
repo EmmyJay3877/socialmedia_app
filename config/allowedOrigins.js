96129b8efd0e931979e812fa10a9bb64c7aad1c0
emmyjay387 <104701190+emmyjay387@users.noreply.github.com>
2023-09-05 16:28:18 +0100

    allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'https://blogapp-api-pdde.onrender.com',
+    process.env.HOST_URL,
     'http://127.0.0.1:5500',
     'http://localhost:8000',
     'http://localhost:3000',
